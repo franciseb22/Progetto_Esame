c3da34964569bcbd07c259913356567297346885
franciseb22 <franciseb22@gmail.com>
2021-04-20 11:17:36 +0100

    aggiunta print

--- src/progettogithub/ProgettoGitHub.java
+++ src/progettogithub/ProgettoGitHub.java
@@ -16,6 +16,7 @@ public class ProgettoGitHub {
      */
     public static void main(String[] args) {
         System.out.println("CIAO");
+        System.out.println("NUOVA MODIFICA");
     }
     
 }
